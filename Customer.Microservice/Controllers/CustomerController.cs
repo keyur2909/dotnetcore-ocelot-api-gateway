d4fda54a6e2b669457b668165fea7be7025f30c3
keyur patel <kppatel9409402056@gmail.com>
2021-05-01 09:59:05 +0530

    Add project files.

---
+++ Customer.Microservice/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Customer.Microservice.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		// GET: api/<CustomerController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "Jane Merry", "Peter Parker" };
+		}
+
+		// GET api/<CustomerController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<CustomerController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+
+		}
+	}
+}
---
+++ Product.Microservice/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Product.Microservice.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+		// GET: api/<ProductController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "Product One", "Product Two" };
+		}
+
+		// GET api/<ProductController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "Procuct Id :" + id;
+		}
+
+		// POST api/<ProductController>
+		[HttpPost]
+		public string Post([FromBody] string value)
+		{
+			return value;
+		}
+	}
+}
